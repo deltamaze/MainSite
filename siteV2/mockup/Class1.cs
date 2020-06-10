e428dbffe550d133d54510e8d00d7b3e7338acd7
deltamaze <wcpooley@gmail.com>
2020-06-09 23:18:19 -0500

    started on v2

---
+++ siteV2/mockup/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic
+
+public class WPooleyWebsite
+{
+    // Introduction
+    string name = "William Pooley";
+    string title = "dot net developer";
+
+    // key = live app
+    // value = source code
+    List<Dictionary> featuresApps = new List<Dictionary>();
+}

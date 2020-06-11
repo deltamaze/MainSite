6e889bf481e9a0724034fc36c4b9e40548dcd65f
deltamaze <wcpooley@gmail.com>
2020-06-10 21:41:40 -0500

    push shortcut

--- siteV2/mockup/Class1.cs
+++ siteV2/mockup/Class1.cs
@@ -1,12 +1,92 @@
-﻿using System.Collections.Generic
-
-public class WPooleyWebsite
+﻿using System.Collections.Generic;
+public class Portfolio
 {
-    // Introduction
+    
     string name = "William Pooley";
-    string title = "dot net developer";
+    string title = "Software Developer";
 
-    // key = live app
-    // value = source code
-    List<Dictionary> featuresApps = new List<Dictionary>();
+    string aboutMe =
+        @"
+            Software developer with over 10 years of professional experience.
+            On my free time, I play around with emerging web technologies.
+        ";
+
+    List<string> skills = new List<string>(){
+            "TSQL",
+            "C#",
+            ".NET Framework/Core",
+            "IIS",
+            "Sql Server Administration Tasks",
+            "SSRS (Report Development)",
+            "SSIS (ETL Services)",
+            "HTML 5/CSS",
+            "JS Libraries/Frameworks: Bootstrap/React/Angular/Firebase",
+        };
+
+    FeaturedApps.Push(
+        new FeaturedApp
+        {
+            Preview = "website.url.com",
+            SourceCode = "github.com/",
+            Description = 
+            @"
+                Timer/Stop watch website built with C# Blazor
+                Targetting Web Assembly, to run natively on client computer
+            "
+        });
+
+    FeaturedApps SimplePlanner =
+        new FeaturedApps
+        {
+            Preview = "planner.wpooley.com", //Display with HTML a tag: "View" with href to actual
+            SourceCode = "github.com/",
+            Description =
+            @"
+                Planner/Calendar website built with Redux/React/Firebase.
+            "
+        };
+
+    FeaturedApps PrivacyScreen =
+        new FeaturedApps
+        {
+            Preview = "planner.wpooley.com",
+            SourceCode = "github.com/",
+            Description =
+            @"
+                Planner/Calendar website built with Redux/React/Firebase.
+            "
+        };
+
+    
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///                          Other Coding Projects                      ///
+    ///
+    /// Project Name:
+    /// Project Type:
+    /// Description:
+    /// Github:
+    /// 
+    /// Project Name:
+    /// Project Type:
+    /// Description:
+    /// Github:
+    /// 
+    /// Project Name:
+    /// Project Type:
+    /// Description:
+    /// Github:
+}
+
+
+public class FeaturedApp
+{
+    public string Preview { get; set; }
+    public string SourceCode{ get; set; }
+    public string Description { get; set; }
+}
+public class Portfolio
+{
+    public List<FeaturedApp> FeaturedApps { get; set; }
+
 }
